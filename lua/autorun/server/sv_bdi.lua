30f3716feb4dd3f502e3e4bcd9ef318063b07ba7
BurgerLUA <mattwasnothere@gmail.com>
2016-02-14 19:31:25 -0800

    Error Stuff

--- lua/autorun/server/sv_bdi.lua
+++ lua/autorun/server/sv_bdi.lua
@@ -4,7 +4,13 @@ util.AddNetworkString( "BDI_SendToClient" )
 
 function BDI_ScalePlayerDamage(victim,hitgroup,dmginfo)
 
-	local Position = dmginfo:GetAttacker():GetShootPos() or dmginfo:GetAttacker():GetPos() + dmginfo:GetAttacker():OBBCenter()
+	local RealAttacker = dmginfo:GetAttacker()
+	
+	if not RealAttacker then
+		RealAttacker = dmginfo:GetInflictor()
+	end
+
+	local Position = RealAttacker:GetPos() + RealAttacker:OBBCenter()
 	local Damage = dmginfo:GetDamage()
 	
 	net.Start("BDI_SendToClient")
