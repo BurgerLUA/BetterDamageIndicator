f72918ce959ccb166bf5f2fa0aad4fca3a0c4e8a
BurgerLUA <mattwasnothere@gmail.com>
2016-03-29 20:28:23 -0700

    Time stuff

--- lua/autorun/client/cl_bdi.lua
+++ lua/autorun/client/cl_bdi.lua
@@ -9,7 +9,7 @@ net.Receive("BDI_SendToClient", function()
 	local Position = net.ReadVector()
 	local Damage = net.ReadFloat()
 	
-	StoredHitInformation[CurTime()] = {Position = Position, Damage = Damage}
+	StoredHitInformation[CurTime() + 2] = {Position = Position, Damage = Damage}
 
 end)
 
